e8e032dd04377429072251447d8f412348b96484
Docker <docker@example.com>
2020-06-24 17:00:40 +0100

    Migrate to pure Irmin

--- src/server.mli
+++ src/server.mli
@@ -6,11 +6,10 @@
 module Make 
   (S: Cohttp_lwt.S.Server)
   (SEC : Mirage_kv.RO)
-  (FS: Mirage_kv.RO)
   (R : Resolver_lwt.S)
   (C : Conduit_mirage.S) 
   (P : Mirage_clock.PCLOCK) :
 sig 
   type s = Conduit_mirage.server -> S.t -> unit Lwt.t
-  val start: s -> SEC.t -> FS.t -> Resolver_lwt.t -> Conduit_mirage.t -> unit -> unit Lwt.t
+  val start: s -> SEC.t -> Resolver_lwt.t -> Conduit_mirage.t -> unit -> unit Lwt.t
 end 
